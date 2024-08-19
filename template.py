a0e6fe82232f60cdb1cb0efc01901074230bc498
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-08-19 16:00:38 +0530

    Environment ready

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:') 
-project_name = "mlops" 
+project_name = "MLOps" 
 list_of_files = [ ".github/workflows/.gitkeep", 
                  f"src/{project_name}/_init.py", 
                  f"src/{project_name}/components/init.py", 
